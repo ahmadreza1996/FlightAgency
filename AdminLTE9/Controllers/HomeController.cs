99d2744b1499db0b747addb552163ede59e4c894
ahmadreza1996 <ahmadreza.anisi1996@outlook.com>
2018-09-27 11:01:33 -0700

    befor change Database

--- AdminLTE9/Controllers/HomeController.cs
+++ AdminLTE9/Controllers/HomeController.cs
@@ -1,9 +1,19 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AdminLTE9.Models;
 
 namespace AdminLTE9.Controllers
 {
     public class HomeController : Controller
     {
+        private FlightAgencyEntities db = new FlightAgencyEntities();
+
         public ActionResult Index()
         {
             return View();
@@ -17,6 +27,32 @@ namespace AdminLTE9.Controllers
         public ActionResult AboutUs()
         {
             return View();
+        }
+
+        public ActionResult Flights()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Flights(string Source, string Destination, DateTime Date, string Class, int AdultNumber, int KidNumber, int LarvaNumber)
+        {
+            string a = Source;
+            return View();
+        }
+
+        public bool IsFlightExist(string Source, string Destination, DateTime Date, string Class, int AdultNumber, int KidNumber, int LarvaNumber)
+        {
+            var check = db.Flights.Where(a => a.F_Origin == Source && a.F_Destination == Destination && a.F_Date == Date).FirstOrDefault();
+            var checkCapacity = db.Airplanes.Where(b => b.A_Capacity == AdultNumber + KidNumber + LarvaNumber).FirstOrDefault();
+            if (check != null && checkCapacity != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
