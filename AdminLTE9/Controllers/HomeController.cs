aaf09c60ff40c587ca6b2e3faa0d6e3a5f063808
ahmadreza1996 <ahmadreza.anisi1996@outlook.com>
2018-11-30 19:54:29 +0330

    not true

--- AdminLTE9/Controllers/HomeController.cs
+++ AdminLTE9/Controllers/HomeController.cs
@@ -101,9 +101,13 @@ namespace AdminLTE9.Controllers
             List<Flight> SearchedFlightsList = new List<Flight>();
 
 <<<<<<< HEAD
+<<<<<<< HEAD
             //var checkFlightExist = db.Flights.Where(a => a.F_Origin == Source && a.F_Destination == Destination && a.F_Date == Date && a.F_Class == Class && a.F_Capacity > AdultNumber + KidNumber + LarvaNumber).FirstOrDefault();
             var checkFlightExist = db.Flights.Where(a => a.F_Class == Class).FirstOrDefault();
 
+=======
+            var checkFlightExist = db.Flights.Where(a => a.F_Origin == Source && a.F_Destination == Destination && a.F_Date == Date && a.F_Capacity > AdultNumber + KidNumber + LarvaNumber).FirstOrDefault();
+>>>>>>> parent of e6a38ea... First after accept
 =======
             var checkFlightExist = db.Flights.Where(a => a.F_Origin == Source && a.F_Destination == Destination && a.F_Date == Date && a.F_Capacity > AdultNumber + KidNumber + LarvaNumber).FirstOrDefault();
 >>>>>>> parent of e6a38ea... First after accept
